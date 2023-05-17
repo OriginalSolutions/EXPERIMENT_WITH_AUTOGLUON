0a8658b1fbad94ba6a5ba63ea9f82174eef7ad42
OriginalSolutions <113025841+OriginalSolutions@users.noreply.github.com>
2023-05-17 21:06:33 +0200

    Update functions_autogluon.py

--- functions_autogluon.py
+++ functions_autogluon.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3.8
-#
 import pandas as pd
 from datetime import datetime
-import copy
-import requests    
+import requests  
 
 BASE_URL = "https://api.gateio.ws"
 CONTEX = "/api/v4"
